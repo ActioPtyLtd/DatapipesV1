9bdec04f5923e294e89ec11c0dd0812c98024a75
Maurice <maurice@actio.com.au>
2016-04-21 08:33:33 +1000

    fixed error with query template config

--- src/main/java/com/actio/TaskLookup.scala
+++ src/main/java/com/actio/TaskLookup.scala
@@ -12,13 +12,14 @@ class TaskLookup extends Task {
   import DataSetTableScala._
 
   def execute() = {
+    super.setConfig(sysconf.getTaskConfig(this.node.getName).toConfig, sysconf.getMasterConfig)
 
     val dataSourceConfig = config.getConfig(DPSystemConfigurable.DATASOURCE_LABEL)
-    val query = dataSourceConfig.getString("query")
+    val query = dataSourceConfig.getString("query.queryTemplate")
 
-    val inClause = dataSet.getColumnValues(lookupColumn1).distinct mkString ","
+    val inClause = dataSet.getColumnValues(lookupColumn1).distinct.map("\'" + _ + "\'") mkString ","
 
-    val dataSource = DPSystemFactory.newDataSource(dataSourceConfig.withValue("query", ConfigValueFactory.fromAnyRef(query.replaceAllLiterally("$1", inClause))), masterConfig)
+    val dataSource = DPSystemFactory.newDataSource(dataSourceConfig.withValue("query.queryTemplate", ConfigValueFactory.fromAnyRef(query.replaceAllLiterally("$1", inClause))), masterConfig)
     dataSource.execute()
 
     val condition = (row1: List[String], row2: List[String]) => row1(dataSet.getOrdinalOfColumn(lookupColumn1)) == row2(dataSource.dataSet.getOrdinalOfColumn(lookupColumn2))

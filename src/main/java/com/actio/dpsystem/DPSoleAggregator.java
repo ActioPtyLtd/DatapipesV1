eeed93b3a492aa744c1219db14fc7a6635437745
Jim Popov <jim@actio.com.au>
2016-07-26 10:38:01 +1000

    commit for event server

---
+++ src/main/java/com/actio/dpsystem/DPSoleAggregator.java
@@ -0,0 +1,10 @@
+package com.actio.dpsystem;
+
+/**
+ * Created by jim on 25/07/16.
+ */
+public class DPSoleAggregator extends DPSystemConfigurable {
+
+    static public DPEventAggregator events = new DPEventAggregator(getUUID());
+
+}

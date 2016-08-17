a545beb2fdc79ac1c5aac734a308ef524897dae6
Maurice <maurice@actio.com.au>
2016-08-18 01:44:32 +1000

    Make lookup task work on generic data sets

--- src/main/java/com/actio/TaskLookup.scala
+++ src/main/java/com/actio/TaskLookup.scala
@@ -13,42 +13,15 @@ class TaskLookup extends Task {
   def execute(): Unit = {
     super.setConfig(sysconf.getTaskConfig(this.node.getName).toConfig, sysconf.getMasterConfig)
 
-    val dataSourceConfig = config.getConfig(DPSystemConfigurable.DATASOURCE_LABEL)
-    val query = dataSourceConfig.getString("query.queryTemplate")
+    dataSet = DataRecord(List(DataArray(dataSet.elems.map(d =>
+      DataRecord(List(d,DataArray(dataSource.read(d).elems.toList)))).toList)))
 
-    val tabledataset = DataSetTableScala(dataSet.schema, dataSet.elems.next) //assuming one batch
-
-    val inClause = tabledataset.getColumnValues(lookupColumn1).distinct.map("\'" + _.replace("'", "''") + "\'") mkString ","
-
-    val dataSource = DPSystemFactory.newDataSource(
-      dataSourceConfig.withValue("query.queryTemplate", ConfigValueFactory.fromAnyRef(query.replaceAllLiterally("$1", inClause))),
-      masterConfig)
-
-    dataSource.execute()
-
-    val ord = dataSource.dataSet.schema.asInstanceOf[SchemaArray].content.asInstanceOf[SchemaRecord].
-      fields.map(_.label).indexOf(lookupColumn2) // assuming tablular
-
-    val condition = (row1: List[String], row2: List[String]) =>
-      if (row2 == Nil) false else row1(tabledataset.getOrdinalOfColumn(lookupColumn1)) == row2(ord)
-
-    val dataList = dataSource.dataSet.elems.toList
-
-    for (data <- dataList) {
-      //TODO: union the resultsets instead of overwriting them here
-      dataSet = DataSetTransforms.transformLookupFunc(tabledataset, DataSetTableScala(dataSource.dataSet.schema, data), condition, _ => true)
-    }
-
-    // nothing found, but make sure columns found in the data source schema gets added to the incoming table
-    if (dataList.isEmpty) {
-      dataSet = DataSetTransforms.transformLookupFunc(tabledataset, DataSetTableScala(dataSource.dataSet.schema, DataArray(Nil)), condition, _ => true)
-    }
   }
+
+  lazy val dataSource: DataSource = DPSystemFactory.newDataSource(config.getConfig(DPSystemConfigurable.DATASOURCE_LABEL), masterConfig)
 
   def load(): Unit = ???
 
   def extract(): Unit = ???
 
-  def lookupColumn1: String = getConfig.getString("lookup1")
-  def lookupColumn2: String = getConfig.getString("lookup2")
 }

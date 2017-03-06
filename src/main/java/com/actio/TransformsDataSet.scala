ac04129da1c49ac91a221f3b9df5a00b9024790b
Maurice Muschalik <maurice@actio.com.au>
2017-03-06 12:00:25 +1100

    fix null issue in strContains

--- src/main/java/com/actio/TransformsDataSet.scala
+++ src/main/java/com/actio/TransformsDataSet.scala
@@ -261,9 +261,9 @@ object TransformsDataSet {
 
   def contains(ds: List[DataSet], d: DataSet) = DataBoolean(ds.exists(i => i.stringOption == d.stringOption))
 
-  def strContains(str: String, targetStr: String): DataSet = DataBoolean(if(str == null) false else str.contains(targetStr))
+  def strContains(str: String, targetStr: String): DataSet = DataBoolean(if(str == null || targetStr == null) false else str.contains(targetStr))
 
-  def strNotContains(str: String, targetStr: String): DataSet = DataBoolean(if(str == null) false else !str.contains(targetStr))
+  def strNotContains(str: String, targetStr: String): DataSet = DataBoolean(if(str == null || targetStr == null) false else !str.contains(targetStr))
 
 
   def substring(str: String, start: Int): DataSet = if (start < str.length) DataString(str.substring(start)) else DataString("")
